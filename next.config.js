54c4c7220283f7387ffdbee7c19e0a5d0a4902c9
Pietro Weg <pietro.sera@compasso.com.br>
2023-01-05 18:19:32 -0300

    :bricks: feat: configurando page-extensions nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'], // quais extensoes de arquivo o next deve considerar como pages para gerar rotas
 }
 
 module.exports = nextConfig
