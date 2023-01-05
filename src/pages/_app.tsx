d7e733f98fb98f52012dbd8493921c5a72d5c328
Pietro Weg <pietro.sera@compasso.com.br>
2023-01-05 17:34:30 -0300

    :lipstick: feat: configurando global css e document com stitches e _document nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+globalStyles(); // Carregado apenas uma única vez
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import { getCssText } from "@ignite-ui/react"
+import { Html, Head, Main, NextScript } from "next/document"
+
+// configurações do documento da aplicação
+// {/* Index html do meu projeto. Apenas coisa que quero em todos meus arquivos/páginas */ }
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* 
+        * Quando o usuário carregar a página, pelo lado do servidar, o cógio css necessário será retornado dessa função; 
+        * Stitches por padrao renderiza o código em run time;
+        * Dessa forma, temos css em ssr
+        */}
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </Head>
+      <body>
+        {/* Em qual lugar da minha app vão os conteúdos das páginas */}
+        <Main />
+        {/* Em qual local do nosso HTML, queremos carregar os scripts da página */}
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+{/* Index html do meu projeto. Apenas coisa que quero em todos meus arquivos/páginas */ }
