26962210fe6ce4cc5ba6aa2e915d062f17b420fa
Pietro Weg <pietro.sera@compasso.com.br>
2023-01-08 21:13:44 -0300

    :construction: feat: interacao de formulario com hook-form e zod schema

--- src/pages/home/components/ClainUsernameForm/index.tsx
+++ src/pages/home/components/ClainUsernameForm/index.tsx
@@ -1,11 +1,32 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+// convertendo estrutura do zod (object, username e string) para uma estrutura do typeScript;
+// inferir (definir de forma automática) o tipo dos dados dentro do form.
+type ClainUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClainUsernameForm() {
+  const { register, handleSubmit } = useForm<ClainUsernameFormData>()
+
+  async function handleClaimUsername(data: ClainUsernameFormData) {
+    console.log(data.username)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
