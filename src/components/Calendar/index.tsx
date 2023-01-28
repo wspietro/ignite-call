d868fd745bf2215d443ba2f5763d2b473f611788
Pietro Weg <pietro.sera@compasso.com.br>
2023-01-28 16:04:20 -0300

    :construcion: feat: estrutura inicial calendar component

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,58 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import { getWeekDays } from '../../utils/get-week-days'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+
+export function Calendar() {
+  const shortWeekDay = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Dezembro <span>2022</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDay.map((weekDay) => {
+              return <th key={weekDay}>{weekDay}.</th>
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>
+            <CalendarDay>1</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>2</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>3</CalendarDay>
+          </td>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
