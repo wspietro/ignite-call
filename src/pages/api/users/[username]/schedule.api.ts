207f117c04ff0f2a1bfeca019ccfbb8b9c16d3a0
Pietro Weg <pietro.sera@compasso.com.br>
2023-02-08 23:18:04 -0300

    :sparkles: feat: criacao do evento no google calendar

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,7 +1,9 @@
 /* eslint-disable camelcase */
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod'
+import { getGoogleOAuthTokern } from '../../../../lib/google'
 import { prisma } from '../../../../lib/prisma'
 
 export default async function handle(
@@ -59,13 +61,44 @@ export default async function handle(
     })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       observations,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  // comunicacao com api do google
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthTokern(user.id),
+  })
+
+  // TODO: como enviar o email do evento? não so marcar no calendario
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
